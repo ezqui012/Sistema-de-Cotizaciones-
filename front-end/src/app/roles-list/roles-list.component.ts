3cdf7f4f57b1d79a472c8006a30774e8e060e77a
ronald quispe espinoza <r24.espinoza@gmail.com>
2021-04-10 12:47:18 -0400

    interface del modulo roles

---
+++ front-end/src/app/roles-list/roles-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-roles-list',
+  templateUrl: './roles-list.component.html',
+  styleUrls: ['./roles-list.component.css']
+})
+export class RolesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
