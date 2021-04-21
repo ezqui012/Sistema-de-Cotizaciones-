fcbfa8bfee57a93e3e6550988b4682804af654b9
ronald quispe espinoza <r24.espinoza@gmail.com>
2021-04-20 23:03:50 -0400

    barra de navegacion

--- front-end/src/app/navbar/navbar.component.ts
+++ front-end/src/app/navbar/navbar.component.ts
@@ -1,20 +1,18 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-navbar',
-  encapsulation:ViewEncapsulation.None,
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private modal: NgbModal) { }
-
+  constructor() { }
+  show: boolean = false;
   ngOnInit(): void {
   }
-  openModal(content: any){
-    this.modal.open(content,{ windowClass:"oscuro"});
-  }
+
 
 }
