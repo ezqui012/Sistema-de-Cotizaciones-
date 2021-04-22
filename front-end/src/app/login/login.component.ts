f891fc465bdbb7906a9514937128e456a6bb140a
Rylmar Quispe <rylm.fqt28@gmail.com>
2021-04-22 17:09:07 -0400

    Implementacion de validaciones de los campos del modulo login

--- front-end/src/app/login/login.component.ts
+++ front-end/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +9,42 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  public isMenuCollapsed = true;
+  private isValidEmail = /\S+@\S+\.\S+/;
+  //public isMenuCollapsed = true;
 
-  constructor() { }
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.pattern(this.isValidEmail)]],
+    password: ['', [Validators.required, Validators.minLength(8)]]
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+  }
+
+  getErrorMessage(field: string){
+    let message;
+    let fieldSpanish = this.getTraduction(field);
+
+    if(this.loginForm.get(field)?.errors?.required){
+      message = `El campo ${fieldSpanish} es obligatorio`;
+    }else if(this.loginForm.get(field)?.hasError('pattern')){
+      message = "Fomrato de correo no valido";
+    } else if(this.loginForm.get(field)?.hasError('minlength')){
+      message = "Ingrese minimo 8 caracteres";
+    }
+
+    return message;
+  }
+
+  isValid(field:string){
+    return ( this.loginForm.get(field)?.touched || this.loginForm.get(field)?.dirty) && !this.loginForm.get(field)?.valid;
+  }
+
+  getTraduction(field: string){
+    return field === 'password' ? 'contraseña' : 'correo';
   }
 
 }
