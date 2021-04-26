bdedc12024daecfd7f6d3fb6d1ec191ac8a71595
Rylmar Quispe <rylm.fqt28@gmail.com>
2021-04-26 18:04:51 -0400

    Implementacion de controlador para registrar nuevas facultades

--- Backe-end/app/Http/Controllers/FacultyController.php
+++ Backe-end/app/Http/Controllers/FacultyController.php
@@ -26,7 +26,19 @@ class FacultyController extends Controller
 
     public function store(Request $request)
     {
-        //
+        try{
+            $input = $request->all();
+            Faculty::create($input);
+            return response()->json([
+                'res' => true,
+                'message' => 'Registered faculty'
+            ], 200);
+        }catch(Exception $ex){
+            return response()->json([
+                'res' => false,
+                'message' => $ex
+            ], 404);
+        }
     }
 
 
