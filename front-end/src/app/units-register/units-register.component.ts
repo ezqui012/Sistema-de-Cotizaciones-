37fcb45226e50d4b07b183b743d147b6f07fd88f
Rylmar Quispe <rylm.fqt28@gmail.com>
2021-04-28 22:32:11 -0400

    Implementacion de validaciones de campos del modulo registro de unidades

--- front-end/src/app/units-register/units-register.component.ts
+++ front-end/src/app/units-register/units-register.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Faculty } from '../Model/faculty';
 import { FacultyService } from '../services/faculty.service';
-import { ValidationAmount } from '../utils/validation-amount';
 
 @Component({
   selector: 'app-units-register',
@@ -16,10 +15,11 @@ export class UnitsRegisterComponent implements OnInit {
   showAmount:boolean=false;
 
   private patternNumber = /^[0-9]+(\.?[0-9]+)?$/;
+  private patternName = /^[a-zA-Z-z0-9-zñÑ\u00E0-\u00FC ]*$/
 
   registerForm = this.fb.group({
     id_faculty: ['', [Validators.required]],
-    name_unit: ['', [Validators.required, Validators.maxLength(100)]],
+    name_unit: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(100), Validators.pattern(this.patternName)]],
     type: ['Administrativa', [Validators.required]]
   });
 
@@ -44,27 +44,69 @@ export class UnitsRegisterComponent implements OnInit {
     );
   }
 
+  isValid(field:string){
+    return ( this.registerForm.get(field)?.touched || this.registerForm.get(field)?.dirty) && !this.registerForm.get(field)?.valid;
+  }
+
+  getErrorMessage(field: string){
+    let message;
+    let fieldSpanish = this.translate(field);
+
+    if(this.registerForm.get(field)?.errors?.required){
+      message = `El campo ${fieldSpanish} es obligatorio`;
+    }else if(this.registerForm.get(field)?.hasError('pattern')){
+      field === 'name_unit' ? message = `El campo ${fieldSpanish} solo acepta caracteres numericos y alfabeticos`
+      : message = `El campo ${fieldSpanish} solo acepta caracteres numericos`;
+    } else if(this.registerForm.get(field)?.hasError('minlength')){
+      const minLength = this.registerForm.get(field)?.errors?.minlength.requiredLength;
+      message = `El campo ${fieldSpanish} requiere como minimo el ingreso de ${minLength} caracteres`;
+    } else if(this.registerForm.get(field)?.hasError('maxlength')){
+      const maxLength = this.registerForm.get(field)?.errors?.maxlength.requiredLength;
+      message = `El campo ${fieldSpanish} permite el ingreso de ${maxLength} caracteres como maximo`;
+    } else if(this.registerForm.get(field)?.hasError('max')){
+      message = `El campo ${fieldSpanish} permite el ingreso maximo de 10 digitos`;
+    }
+
+    return message;
+  }
+
   showInputAmout(show:boolean){
     this.showAmount =show;
     this.registerForm.get('amount')?.reset;
     if(this.showAmount){
       this.registerForm = this.fb.group({
         id_faculty: ['', [Validators.required]],
-        name_unit: ['', [Validators.required, Validators.maxLength(100)]],
+        name_unit: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(100), Validators.pattern(this.patternName)]],
         type: ['Gasto', [Validators.required]],
-        amount: [null, [ValidationAmount.isVisible(this.showAmount), Validators.maxLength(3), Validators.pattern(this.patternNumber)]]
+        amount: ['', [Validators.required, Validators.max(9999999999.99), Validators.pattern(this.patternNumber)]]
       });
     }else{
       this.registerForm = this.fb.group({
         id_faculty: ['', [Validators.required]],
-        name_unit: ['', [Validators.required, Validators.maxLength(100)]],
+        name_unit: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(100), Validators.pattern(this.patternName)]],
         type: ['Administrativa', [Validators.required]]
       });
     }
   }
 
   mostrar(){
+    if(this.registerForm.invalid){
+      console.log("campos invalidos");
+      return;
+    }
     console.log(this.registerForm.value);
+  }
+
+  translate(field: string):string|void{
+    if(field === 'id_faculty'){
+      return 'Facultad';
+    } else if(field === 'name_unit'){
+      return 'Nombre de unidad';
+    } else if(field === 'type'){
+      return 'Tipo de unidad'
+    }else if(field === 'amount'){
+      return 'Monto';
+    }
   }
 
 }
--- front-end/src/app/utils/validation-amount.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ValidationAmount } from './validation-amount';
-
-describe('ValidationAmount', () => {
-  it('should create an instance', () => {
-    expect(new ValidationAmount()).toBeTruthy();
-  });
-});
