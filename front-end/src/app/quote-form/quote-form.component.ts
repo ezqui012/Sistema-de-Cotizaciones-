6e42af9ccea2ac3be8fc8889b3255d19ec9657f3
Rylmar Quispe <rylm.fqt28@gmail.com>
2021-05-16 00:56:17 -0400

    Implementacion de grupo de formulario para capturar datos del formulario de cotizacion

--- front-end/src/app/quote-form/quote-form.component.ts
+++ front-end/src/app/quote-form/quote-form.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-quote-form',
@@ -11,13 +15,33 @@ import {map, startWith} from 'rxjs/operators';
 })
 export class QuoteFormComponent implements OnInit {
 
+  /* Razon social estatica */
+  business_name:string = 'Actualizacion de monitores del laboratorio';
+
   findControl = new FormControl();
   options: string[] = ['Tigo', 'Muebles rr', 'Mundo Tecno', 'Viva', 'Pollos de la case'];
   filteredOptions: Observable<string[]> | undefined;
 
   items: string[] = ['Monitores', 'Sillas'];
 
-  constructor() { }
+  registerForm = this.fb.group({
+    id_enterprise: ['', [Validators.required]],
+    id_item: ['', [Validators.required]],
+    id_quotation: ['', [Validators.required]],
+    quantity: ['', [Validators.required]],
+    unit_cost: ['', [Validators.required]],
+    date:['', [Validators.required]],
+    delivery_days: ['', [Validators.required]]
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    public toastr: ToastrService,
+    private router:Router,
+    private titlePage: Title
+  ) {
+    this.titlePage.setTitle('Formulario de cotización - QUOT-UMSS');
+  }
 
   ngOnInit(): void {
     this.filteredOptions = this.findControl.valueChanges
@@ -31,6 +55,10 @@ export class QuoteFormComponent implements OnInit {
     const filterValue = value.toLowerCase();
 
     return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  showValue(){
+    console.log(this.registerForm.value);
   }
 
 }
