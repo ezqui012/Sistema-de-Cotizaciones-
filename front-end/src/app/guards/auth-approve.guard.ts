d5a59818bb91c17f3b947b3d3c196cef0c1fe5b3
Rylmar Quispe <rylm.fqt28@gmail.com>
2021-05-27 21:45:03 -0400

    Implementacion de Guard de aprovacion y Guard de solicitud

--- front-end/src/app/guards/auth-approve.guard.ts
+++ front-end/src/app/guards/auth-approve.guard.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthApproveGuard implements CanActivate {
+
+  constructor(
+    private router:Router
+  ){}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      let permission = JSON.parse("[" + localStorage.getItem('quot-umss-pa') + "]");
+      if(localStorage.getItem('quot-umss-tk') && localStorage.getItem('quot-umss-p') !== '1' && permission.includes(3)){
+        return true;
+      }else{
+        this.router.navigate(['/home-user']);
+        return false;
+      }
   }
-  
+
 }
