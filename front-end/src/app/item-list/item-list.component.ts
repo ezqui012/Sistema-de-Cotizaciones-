2e12a7b652c4f305c60af40e3a5aa82d260ec576
Rylmar Quispe <rylm.fqt28@gmail.com>
2021-05-31 12:51:07 -0400

    Carga de tabla de lista items, carga del detalle de un item

--- front-end/src/app/item-list/item-list.component.ts
+++ front-end/src/app/item-list/item-list.component.ts
@@ -3,6 +3,8 @@ import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { NgbModal, NgbPopoverConfig } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
+import { RequestQuoteService } from '../services/request.service';
+import { DateExpenseItem } from '../Model/expenseItem';
 
 @Component({
   selector: 'app-item-list',
@@ -13,12 +15,16 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class ItemListComponent implements OnInit {
 
+  itemList: DateExpenseItem | any;
+  pos: any;
+
   constructor(
     private router:Router,
     public toastr: ToastrService,
     private titlePage: Title,
     private modal: NgbModal,
-    public config: NgbPopoverConfig
+    public config: NgbPopoverConfig,
+    private service: RequestQuoteService
   ) {
     this.titlePage.setTitle('Lista de Items - QUOT-UMSS');
     config.placement = 'left';
@@ -26,15 +32,27 @@ export class ItemListComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.loadListItems();
   }
 
   navigateTo(path: String){
     this.router.navigate([path]);
   }
 
-  openModal(content: any){
+  openModal(content: any, pos: number){
     this.modal.open(content,{ windowClass:"colorModal"});
-    //this.pos = pos;
+    this.pos = pos;
+  }
+
+  loadListItems(){
+    this.service.allItem().subscribe(
+      (data) => {
+        this.itemList = data;
+      },
+      (error) => {
+        this.toastr.error(`ERROR: ${error} Regargue la pagina`);
+      }
+    );
   }
 
 }
