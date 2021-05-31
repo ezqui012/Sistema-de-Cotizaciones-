004cece1634a64358c875c828037e8edb6f581e2
ronald quispe espinoza <r24.espinoza@gmail.com>
2021-05-30 21:12:59 -0400

    Componentes de empresa

---
+++ front-end/src/app/company-register/company-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-company-register',
+  templateUrl: './company-register.component.html',
+  styleUrls: ['./company-register.component.css']
+})
+export class CompanyRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
