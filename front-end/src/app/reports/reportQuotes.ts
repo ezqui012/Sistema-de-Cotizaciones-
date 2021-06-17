77be39a399ff5941b05f34e4f2e6486f00c6b7ec
ronald quispe espinoza <r24.espinoza@gmail.com>
2021-06-17 03:43:55 -0400

    formulario pdf de cotizacion

--- front-end/src/app/reports/reportQuotes.ts
+++ front-end/src/app/reports/reportQuotes.ts
@@ -1,11 +1,12 @@
 import { PdfMakeWrapper, Txt, Table } from 'pdfmake-wrapper';
 import { ITable } from 'pdfmake-wrapper/lib/interfaces';
 import { AcceptedQuote } from '../Model/accepted-quote';
+import { ItemRequest } from '../Model/expense-item';
 import { Quote } from '../Model/quoteModel';
 
 
 type TableRow = [number, number, string, string, string, number, number, number];
-//type TableRowQP = [number, number, string, string, string, number, number];
+type TableRowE = [number, number, string, string, string, string, string];
 const tiempo = Date.now();
 const hoy = new Date(tiempo);
 //hoy.toLocaleDateString();
@@ -15,17 +16,7 @@ export class ReportQuotes {
 
   }
 
-  private tableBusinessData(business:string, dateRequest:string): ITable {
-    [{}]
-    return new Table([
-      [new Txt(`Razón social: ${business}`).fontSize(11).alignment('left').end
-      , new Txt(`Fecha: ${dateRequest}`).fontSize(11).alignment('right').end]
-    ])
-      .widths([380, 110])
-      .layout('noBorders')
-      .alignment('center').margin([0,0,0,0])
-      .end
-  }
+
   private tableHeader(nameFaculty:string): ITable {
     [{}]
     return new Table([
@@ -55,6 +46,7 @@ export class ReportQuotes {
   }
 
 
+
   public generateQuotePerformedPdf(
     business: string,
     userName: string,
@@ -73,7 +65,6 @@ export class ReportQuotes {
     const userRequestData = new Txt(`Encargado de la Solicitud: ${userName}`).fontSize(11).alignment('left').end
     const userAceptedData = new Txt(`La Solicitud fue aceptada por: ${userName}`).fontSize(11).alignment('left').end
     const personalData = new Txt(`Encargado de la Cotización: ${personalQuote}`).fontSize(11).alignment('left').end
-    //const userRejectedData = new Txt(`Rechazado por: ${userName}`).fontSize(11).alignment('left').end
 
     pdf.add(this.tableHeader(nameFaculty));
     pdf.add(pdf.ln(2))
@@ -93,6 +84,72 @@ export class ReportQuotes {
     pdf.add(infoRequest);
     pdf.add(personalData);
     pdf.add(statusRequestData);
+    pdf.create().open();
+  }
+  private tableBusinessData(): ITable {
+    [{}]
+    return new Table([
+      [new Txt(`Empresa: ....................................................................................................... `).fontSize(11).alignment('left').end
+      , new Txt(`Fecha: .................................. `).fontSize(11).alignment('left').end]
+    ])
+      .widths([350, 140])
+      .layout('noBorders')
+      .alignment('center').margin([0,0,0,0])
+      .end
+  }
+  private crateTableE(data: ItemRequest[]): ITable {
+    [{}]
+    return new Table([
+      ['Nº', 'Cantidad', 'Unidad', 'Detalle','Dias de entrega', 'Unitario', 'Total'],
+      ...this.extractDataE(data)
+    ])
+      .widths([20, 50, 40, 170, 45, 50, 60])
+      .heights(()=>{return 20})
+      .layout({
+        fillColor:(rowIndex: any, node:any, columnIndex: any) => {
+          return rowIndex === 0 ? '#F4F4F4' : '';
+        },
+        hLineWidth:()=>{
+          return 0.5;
+        },
+        vLineWidth:()=>{
+          return 0.5;
+        },
+        vLineColor:()=>{
+          return '#737171';
+        },
+        paddingTop:()=>{
+          return 4;
+        }
+      })
+      .end;
+  }
+  private extractDataE(data: ItemRequest[]): TableRowE[] {
+    let i = 1;
+    return data.map(row => [(i++), row.quantity, row.unit_item, row.name_item, '','','']);
+  }
+  public generateQuoteEmptyPdf(
+    nameFaculty: string,
+    items: ItemRequest[],
+
+  ): void {
+    const pdf = new PdfMakeWrapper();
+    const title = new Txt('FORMULARIO DE COTIZACIÓN').bold().fontSize(14).alignment('center').end
+    const titleList = new Txt(`Lista de ítems requeridos:`).bold().fontSize(11).alignment('left').end
+
+    pdf.add(this.tableHeader(nameFaculty));
+    pdf.add(pdf.ln(2))
+
+    pdf.add(title)
+    pdf.add(pdf.ln(1))
+    pdf.defaultStyle({
+      fontSize: 11
+    });
+
+    pdf.add(this.tableBusinessData());
+    pdf.add(pdf.ln(2));
+    pdf.add(titleList);
+    pdf.add(this.crateTableE(items));
     pdf.create().open();
   }
 
