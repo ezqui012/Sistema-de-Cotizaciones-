d65163adf87ef018eff65d7b21e45a2cc04975da
Rylmar Quispe <rylm.fqt28@gmail.com>
2021-07-02 22:46:57 -0400

    Se añadio el rol del usuario en la pagina de inicio

--- front-end/src/app/home-admin/home-admin.component.ts
+++ front-end/src/app/home-admin/home-admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import {Router} from '@angular/router';
+import { RolesService } from '../services/roles.service';
 
 @Component({
   selector: 'app-home-admin',
@@ -12,15 +13,23 @@ import {Router} from '@angular/router';
 export class HomeAdminComponent implements OnInit {
 
   nameUser = localStorage.getItem('quot-user') ? localStorage.getItem('quot-user') : '';
+  rolId = Number(localStorage.getItem('quot-umss-p') ? localStorage.getItem('quot-umss-p'): 1);
+  rolUser: any;
 
   constructor(
     private router: Router,
-    private titlePage: Title
+    private titlePage: Title,
+    private service: RolesService
   ) {
     this.titlePage.setTitle('Inicio administrador - QUOT-UMSS');
    }
 
   ngOnInit(): void {
+    this.service.getRoleSelect(this.rolId).subscribe(
+      (data) => {
+        this.rolUser = data.name_role;
+      }
+    );
   }
   navigateTo(path: String){
     this.router.navigate([path]);
